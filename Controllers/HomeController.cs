9604a88c1e94404d2aff5422475410d6a2a67970
SadDexx <75577480+SadDexx@users.noreply.github.com>
2021-03-25 15:21:24 +0300

    test
    tet

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NETCORE.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NETCORE.Interface;
+using System;
+
+namespace NETCORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        IUser usr;
+        public HomeController(IUser u)
+        {
+            usr = u;
+        }
+        public IActionResult Index()
+        {
+            //ViewData["Message"] = "Test";
+            //return View("Index");
+            return View(usr.Alls());
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult GetUser(int? id)
+        {
+            if (!id.HasValue)
+                return BadRequest();
+            User user = usr.Get(id.Value);
+            if (user == null)
+                return NotFound();
+            return View(user);
+        }
+
+        public IActionResult AddUser() => View();
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                usr.Create(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Interface/IUser.cs
@@ -0,0 +1,40 @@
+﻿using NETCORE.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCORE.Interface
+{
+    public interface IUser
+    {
+        List<User> Alls();
+        User Get(int id);
+        void Create(User user);
+    }
+    public class users : IUser
+    {
+        public List<User> Alls()
+        {
+            var us = new List<User>
+            {
+                new User { Id=1, Name="Tom", Age=35},
+                new User { Id=2, Name="Alice", Age=29},
+                new User { Id=3, Name="Sam", Age=32},
+                new User { Id=4, Name="k", Age=30}
+            };
+            return us;
+        }
+
+        public void Create(User user)
+        {
+            new User { };
+        }
+
+        public User Get(int id)
+        {
+            return Alls().Single(p => p.Id == id);
+        }
+    }
+}
